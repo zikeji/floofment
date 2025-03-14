f3301b99ca505433ef16d16fb324f28e62e542df
Zikeji <me@zikeji.com>
2025-03-14 01:51:16 +0000

    chore: add primevue

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -5,9 +5,64 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
+import PrimeVue from 'primevue/config';
+import { definePreset } from '@primeuix/themes';
+import Lara from '@primeuix/themes/aura';
 import { initializeTheme } from './composables/useAppearance';
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+
+const PrimeVuePreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: '{rose.50}',
+            100: '{rose.100}',
+            200: '{rose.200}',
+            300: '{rose.300}',
+            400: '{rose.400}',
+            500: '{rose.500}',
+            600: '{rose.600}',
+            700: '{rose.700}',
+            800: '{rose.800}',
+            900: '{rose.900}',
+            950: '{rose.950}'
+        },
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{zinc.50}',
+                    100: '{zinc.100}',
+                    200: '{zinc.200}',
+                    300: '{zinc.300}',
+                    400: '{zinc.400}',
+                    500: '{zinc.500}',
+                    600: '{zinc.600}',
+                    700: '{zinc.700}',
+                    800: '{zinc.800}',
+                    900: '{zinc.900}',
+                    950: '{zinc.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{neutral.50}',
+                    100: '{neutral.100}',
+                    200: '{neutral.200}',
+                    300: '{neutral.300}',
+                    400: '{neutral.400}',
+                    500: '{neutral.500}',
+                    600: '{neutral.600}',
+                    700: '{neutral.700}',
+                    800: '{neutral.800}',
+                    900: '{neutral.900}',
+                    950: '{neutral.950}'
+                }
+            }
+        }
+    }
+});
 
 // Extend ImportMeta interface for Vite...
 declare module 'vite/client' {
@@ -43,6 +98,14 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: PrimeVuePreset,
+                    options: {
+                        darkModeSelector: '.dark',
+                    },
+                },
+            })
             .mount(el);
     },
     progress: {
